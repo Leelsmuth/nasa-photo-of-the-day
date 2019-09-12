ec22c4c21f84f1579bfc269bd78de5bddc5aff9b
Leelsmuth <femianjorin@rocketmail.com>
2019-09-12 13:12:40 +0100

    wip

--- src/Card.js
+++ src/Card.js
@@ -1,9 +1,14 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledH1 = styled.h1`
+  color: red;
+`;
 
 const Card = props => {
   return (
     <div className="cards">
-      <h1>NASA photo of the Day</h1>
+      <StyledH1>NASA photo of the Day</StyledH1>
       <h1>{props.title}</h1>
       <h2>{props.ImgDate}</h2>
       <img className="img" src={props.imgUrl} />
